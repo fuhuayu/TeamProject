b204719fb5058944497a72f8e348d0259ebed2a8
fuhuayu <howardfu@udel.edu>
2015-11-01 19:36:42 -0500

    change1

---
+++ src/Game2/Sun.java
@@ -0,0 +1,10 @@
+package Game2;
+
+public class Sun extends MovingObject {
+
+	public Sun(int x, int y, int size) {
+		super(x, y, size);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/Game2Test/TestMovingObject.java
@@ -0,0 +1,21 @@
+package Game2Test;
+
+import static org.junit.Assert.*;
+
+import java.awt.Point;
+
+import org.junit.Test;
+
+import Game2.MovingObject;
+
+public class TestMovingObject {
+
+	@Test
+	public void test() {
+		//test constructor
+		MovingObject m=new MovingObject(0, 0, 0);
+		assertEquals(new Point(0,0),m.getPosition());
+		assertEquals(0,m.getSize());
+	}
+
+}
