46595687e45dd3e2fba4989e6462317bd82c070b
fuhuayu <howardfu@udel.edu>
2015-11-01 19:59:38 -0500

    2

--- src/Game2Test/TestMovingObject.java
+++ src/Game2Test/TestMovingObject.java
@@ -16,10 +16,14 @@ public class TestMovingObject {
 		MovingObject m=new MovingObject(0, 0, 0);
 		assertEquals(new Point(0,0),m.getPosition());
 <<<<<<< HEAD
+<<<<<<< HEAD
 		assertEquals(0,m.getSize());//h
 =======
 		assertEquals(0,m.getSize());
 >>>>>>> parent of 6b0f76b... 1
+=======
+		assertEquals(0,m.getSize());//hh
+>>>>>>> parent of f14858a... yo
 	}
 
 }
